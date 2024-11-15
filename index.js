2c9f8abc4cda15e981fad3c509dc35feb58767c5
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-11-15 15:12:58 +0300

    feat: add calculator function

---
+++ index.js
@@ -0,0 +1,35 @@
+function reversePolishCalculator(string) {
+    console.log('Hello! This is calculator of reverse polish notation version 1.0.0')
+  if (typeof string !== "string") {
+    console.log("an argument should be a string");
+  }
+  const symbols = string.split(" ");
+  const stack = [];
+
+  symbols.forEach((symbol) => {
+    if (!isNaN(symbol)) {
+      stack.push(Number(symbol));
+    } else if (checkIsOperator(symbol)) {
+      const b = stack.pop();
+      const a = stack.pop();
+      switch (symbol) {
+        case "+":
+          stack.push(a + b);
+          break;
+        case "-":
+          stack.push(a - b);
+          break;
+        case "*":
+          stack.push(a * b);
+          break;
+      }
+    }
+  });
+  return stack.pop();
+
+  function checkIsOperator(symbol) {
+    return "+-*".includes(symbol);
+  }
+}
+
+module.exports = reversePolishCalculator 
