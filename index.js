0f3046dd54de242ab5e228a462351805e5d74b40
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-11-18 14:00:10 +0300

    feat: make operations a separate object as minor change

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
 function reversePolishCalculator(string) {
-    console.log('Hello! This is calculator of reverse polish notation version 1.0.0')
-  if (typeof string !== "string") {
-    throw new Error("an argument should be a string");
-  }
-  const symbols = string.split(" ");
-  const stack = [];
-
-  symbols.forEach((symbol) => {
-    if (!isNaN(symbol)) {
-      stack.push(Number(symbol));
-    } else if (checkIsOperator(symbol)) {
-      const b = stack.pop();
-      const a = stack.pop();
-      switch (symbol) {
-        case "+":
-          stack.push(a + b);
-          break;
-        case "-":
-          stack.push(a - b);
-          break;
-        case "*":
-          stack.push(a * b);
-          break;
-      }
+    console.log('Hello! This is calculator of reverse polish notation version 1.1.0');
+    if (typeof string !== "string") {
+      throw new Error("an argument should be a string");
     }
-  });
-
-  if (stack.length != 1) {
-    throw new Error('The calculator broke down while calculating');
-}
-  return stack.pop();
-
-}
-
-function checkIsOperator(symbol) {
+    const symbols = string.split(" ");
+    const stack = [];
+  
+    const operations = {
+      "+": (a, b) => a + b,
+      "-": (a, b) => a - b,
+      "*": (a, b) => a * b
+    };
+  
+    symbols.forEach((symbol) => {
+      if (!isNaN(symbol)) {
+        stack.push(Number(symbol));
+      } else if (checkIsOperator(symbol)) {
+        const b = stack.pop();
+        const a = stack.pop();
+        stack.push(operations[symbol](a, b));
+      }
+    });
+  
+    if (stack.length !== 1) {
+      throw new Error('The calculator broke down while calculating');
+    }
+  
+    return stack.pop();
+  }
+  
+  function checkIsOperator(symbol) {
     return "+-*".includes(symbol);
   }
-
-module.exports = reversePolishCalculator 
+  
+  module.exports = reversePolishCalculator;
+  
